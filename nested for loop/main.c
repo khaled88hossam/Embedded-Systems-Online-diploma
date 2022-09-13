199adc013e08943c31718de79d018199925df0de
khaled88hossam <kh851564@gmail.com>
2022-09-13 19:15:52 +0200

    nested for loop

---
+++ nested for loop/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: ١٣‏/٠٩‏/٢٠٢٢
+ *      Author: User
+ */
+#include <stdio.h>
+void main()
+{
+	int i=0,j=1;
+	for(i=0;i<3;i++)
+	{
+		for(j=1;j<=3;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
+
